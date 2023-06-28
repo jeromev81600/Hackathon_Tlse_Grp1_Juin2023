430dfb3209a59c9416beb9610dfc1b31ec77381d
AnneClr <anneclaire.fougerit@gmail.com>
2023-06-28 22:59:59 +0200

    page not found mobile done

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,8 +1,26 @@
+import "../styles/NotFoundPage.scss";
+
 export default function NotFoundPage() {
   return (
     <>
-      <p>Ooops</p>
-      <h1>Erreur 404</h1>
+      <div className="header-text">
+        <p>Ooops ...</p>
+      </div>
+      <div className="img-not-found" />
+      <h1 className="title404">Erreur 404</h1>
+      <section className="text404">
+        <p>
+          La page que vous recherchez n'est pas disponible
+          <div className="emoji-sad" />
+        </p>
+        <p>
+          En{" "}
+          <a href="/#" className="home-link">
+            cliquant ici
+          </a>
+          , vous retournerez à la page principale
+        </p>
+      </section>
     </>
   );
 }
