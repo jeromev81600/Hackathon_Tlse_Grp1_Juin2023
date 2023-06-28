1d8a7315d7fcb8f67d84567c160997a08c448908
jerome vilanova <jerome.vilanova@gmail.com>
2023-06-28 15:50:17 +0200

    Route backend User

---
+++ backend/src/models/UserManager.js
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+const AbstractManager = require("./AbstractManager");
+
+class UserManager extends AbstractManager {
+  constructor() {
+    super({ table: "user" });
+  }
+
+  insert(user) {
+    const { firstname, lastname, email, hashed_password, is_admin, factory } =
+      user;
+    return this.database.query(
+      `insert into ${this.table} ( firstname,
+      lastname,
+      email,
+      hashed_password,
+      is_admin,
+      factory) values (?,?,?,?,?,?)`,
+      [firstname, lastname, email, hashed_password, is_admin, factory]
+    );
+  }
+
+  update(user, id) {
+    const { firstname, lastname, email, hashed_password, is_admin, factory } =
+      user;
+    return this.database.query(
+      `update ${this.table} set firstname = ?, lastname = ?, email = ?, hashed_password = ?, is_admin = ?, factory = ? where id = ?`,
+      [firstname, lastname, email, hashed_password, is_admin, factory, id]
+    );
+  }
+}
+
+module.exports = UserManager;
--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,22 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const UserControllers = require("./controllers/UserControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+// ----------------------------------------- Users routes -------------------------------------------
+
+// ----------------------------------------- Admin routes ------------------------------------------------
+
+router.get("/user", UserControllers.browse);
+router.get("/user/:id", UserControllers.read);
+router.put("/user/:id", UserControllers.edit);
+router.post("/user", UserControllers.add);
+router.delete("/user/:id", UserControllers.destroy);
 
 module.exports = router;
