c7674796ae1f4874bf99353a6ea0daf1c32afed1
CedricPalacio <cedric.palacio@gmail.com>
2023-06-29 14:03:54 +0200

    front_homePage_componants- add img homde page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,29 @@
 import Footer from "../../components/Footer/Footer";
-import NavBar from "../../components/NavBar/NavBar";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Home.scss";
+import cardhomeadd from "../../assets/image/img-ajout-appareil.svg";
+import cardhomeview from "../../assets/image/img-acceder-bdd.svg";
 
 export default function Home() {
   return (
     <header className="App-header">
-      <NavBar />
+      <Navbar />
+      <main>
+        <div className="container-big-button">
+          <div className="card-button-home">
+            <div className="card-home">
+              <img src={cardhomeadd} alt="img-ajout-appareil" />
+            </div>
+            <p>Ajouter un nouvel appareil</p>
+          </div>
+          <div className="card-button-home">
+            <div className="card-home">
+              <img src={cardhomeview} alt="img-ajout-appareil" />
+            </div>
+            <p>Accéder au stock</p>
+          </div>
+        </div>
+      </main>
       <Footer />
     </header>
   );
