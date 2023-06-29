883cc145b560c4ef7107b748ea84569696e4b623
CedricPalacio <cedric.palacio@gmail.com>
2023-06-29 15:43:44 +0200

    front_homePage_componants-finish navbar without link

--- frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import NavBar from "../../components/NavBar/NavBar";
+import NavBar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import "./NotFoundPage.scss";
 
