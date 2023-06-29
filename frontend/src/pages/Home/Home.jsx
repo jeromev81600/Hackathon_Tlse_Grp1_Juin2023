6aff1abb529ba7d8834b09c5494b08adebb54684
CedricPalacio <cedric.palacio@gmail.com>
2023-06-29 16:21:06 +0200

    front_homePage_componants-finish without route

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
-// import { useContext } from "react";
-// import AuthFunctionContext from "../../contexts/AuthFunctionContext";
+import { useContext } from "react";
+import AuthFunctionContext from "../../contexts/AuthFunctionContext";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Home.scss";
@@ -7,33 +7,33 @@ import cardhomeadd from "../../assets/image/img-ajout-appareil.svg";
 import cardhomeview from "../../assets/image/img-acceder-bdd.svg";
 
 function Home() {
-  // const { userToken } = useContext(AuthFunctionContext);
+  const { userToken } = useContext(AuthFunctionContext);
 
   return (
     // (userInfo.is_admin === 0 || userInfo.is_admin === 1) &&
-    // userToken && (
-    <div className="home">
-      <Navbar />
-      <main>
-        <div className="container-big-button">
-          <div className="card-button-home">
-            <div className="card-home">
-              <img src={cardhomeadd} alt="img-ajout-appareil" />
+    userToken && (
+      <div className="home">
+        <Navbar />
+        <main>
+          <div className="container-big-button">
+            <div className="card-button-home">
+              <div className="card-home">
+                <img src={cardhomeadd} alt="img-ajout-appareil" />
+              </div>
+              <p>Ajouter un nouvel appareil</p>
             </div>
-            <p>Ajouter un nouvel appareil</p>
-          </div>
-          <div className="card-button-home">
-            <div className="card-home">
-              <img src={cardhomeview} alt="img-ajout-appareil" />
+            <div className="card-button-home">
+              <div className="card-home">
+                <img src={cardhomeview} alt="img-ajout-appareil" />
+              </div>
+              <p>Accéder au stock</p>
             </div>
-            <p>Accéder au stock</p>
           </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
+        </main>
+        <Footer />
+      </div>
+    )
   );
-  // );
 }
 
 export default Home;
