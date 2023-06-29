9f1369628ae4d52cd195ec684c113e307244e062
AnneClr <anneclaire.fougerit@gmail.com>
2023-06-29 12:23:31 +0200

    fix navbar name

--- frontend/src/pages/EspaceAdmin/EspaceAdmin.jsx
+++ frontend/src/pages/EspaceAdmin/EspaceAdmin.jsx
@@ -1,4 +1,4 @@
-import NavBar from "../../components/NavBar/Navbar";
+import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 
 function EspaceAdmin() {
