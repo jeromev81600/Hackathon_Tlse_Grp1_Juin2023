0400cee481afefad1eb68c2879583564202e1b90
ANTOINE_CASSAGNE <cassagnea@gmail.com>
2023-06-29 14:44:12 +0200

    component_stock

--- frontend/src/components/StockComponent/StockComponent.jsx
+++ frontend/src/components/StockComponent/StockComponent.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useMediaQuery } from "@material-ui/core";
+
 import "./StockComponent.scss";
 
 function StockPhones() {
   const [phones, setPhones] = useState([]);
+
+  const isLargeScreen = useMediaQuery("(min-width: 769px)");
 
   const fetchStockPhones = async () => {
     try {
@@ -46,35 +51,67 @@ function StockPhones() {
   }, []);
 
   return (
-    <div>
-      <ul>
-        {phones.map((phone) => (
-          <li key={phone.id}>
-            <div className="phone-card">
-              <div className="photo-content">
-                <img
-                  className="photo"
-                  src={phone.brand_name.url_logo}
-                  alt="model"
+    <div className="table-container">
+      {isLargeScreen ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Logo</th>
+              <th>Marque</th>
+              <th>Modèle</th>
+              <th>Prix</th>
+              <th>Lieu</th>
+              <th>Chargeur</th>
+            </tr>
+          </thead>
+          <tbody>
+            {phones.map((phone) => (
+              <tr key={phone.id}>
+                <td>{phone.brand_name.url_logo}</td>
+                <td>{phone.brand_name}</td>
+                <td>{phone.model_name}</td>
+                <td>{phone.price}</td>
+                <td>{phone.factory}</td>
+                <td
+                  className={`charger ${
+                    phone.has_charger ? "has-charger" : "no-charger"
+                  }`}
                 />
-              </div>
-              <div className="info">
-                <p>Marque : {phone.brand_name}</p>
-                <p className="label">Modèle : {phone.model_name}</p>
-                <p>Prix : {phone.price}</p>
-                <p>Lieu : {phone.factory}</p> {/* Display factory name */}
-              </div>
-              <p
-                className={`charger ${
-                  phone.has_charger ? "has-charger" : "no-charger"
-                }`}
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div>
+          <ul>
+            {phones.map((phone) => (
+              <li key={phone.id}>
+                <div className="phone-card">
+                  <div className="photo-content">
+                    <img
+                      className="photo"
+                      src={phone.brand_name.url_logo}
+                      alt="model"
+                    />
+                  </div>
+                  <div className="info">
+                    <p>Marque : {phone.brand_name}</p>
+                    <p className="label">Modèle : {phone.model_name}</p>
+                    <p>Prix : {phone.price}</p>
+                    <p>Lieu : {phone.factory}</p>
+                  </div>
+                  <p
+                    className={`charger ${
+                      phone.has_charger ? "has-charger" : "no-charger"
+                    }`}
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
-
 export default StockPhones;
