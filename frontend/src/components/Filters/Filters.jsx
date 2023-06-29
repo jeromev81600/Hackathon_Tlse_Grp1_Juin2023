6851a42c4fb43997ad4068288150c8cbb405240b
jerome vilanova <jerome.vilanova@gmail.com>
2023-06-29 18:37:59 +0200

    Filters

---
+++ frontend/src/components/Filters/Filters.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbarold/Navbar";
+import Footer from "../Footer/Footer";
+import "./Filters.scss";
+
+export default function Filters() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
