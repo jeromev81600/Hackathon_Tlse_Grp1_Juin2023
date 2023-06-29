c9ad3e44f8a4dbb8fe9c31cb2f31d143e558187d
Antoine JULIAN <antoine.julian@hotmail.com>
2023-06-29 14:38:40 +0200

    route smartphoneDetails

--- backend/src/models/SmartphoneManager.js
+++ backend/src/models/SmartphoneManager.js
@@ -6,6 +6,41 @@ class SmartphoneManager extends AbstractManager {
     super({ table: "smartphone" });
   }
 
+  details(id) {
+    return this.database.query(
+      `SELECT
+      s.id,
+      s.user_id,
+      s.reserve,
+      s.created_date,
+      s.has_charger,
+      s.weighted_total,
+      b.brand,
+      b.url_logo,
+      m.model,
+      os.operating_system,
+      sc.screen,
+      n.network,
+      st.storage,
+      r.ram,
+      w.weighting,
+      w.state,
+      s.price,
+      s.url_phone
+    FROM ${this.table} s
+    JOIN brand b ON s.brand_id = b.id
+    JOIN model m ON s.model_id = m.id
+    JOIN operating_system os ON s.operating_system_id = os.id
+    JOIN screen sc ON s.screen_id = sc.id
+    JOIN network n ON s.network_id = n.id
+    JOIN storage st ON s.storage_id = st.id
+    JOIN ram r ON s.ram_id = r.id
+    JOIN weighting w ON s.weighting_id = w.id
+    WHERE s.id = ?`,
+      [id]
+    );
+  }
+
   insert(smartphone) {
     const {
       user_id,
