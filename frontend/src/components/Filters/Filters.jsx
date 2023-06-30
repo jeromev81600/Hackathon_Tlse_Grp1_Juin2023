f506512e5c44291cb372100f40730fd4140a9e9b
jerome vilanova <jerome.vilanova@gmail.com>
2023-06-30 08:27:10 +0200

    Filters

--- frontend/src/components/Filters/Filters.jsx
+++ frontend/src/components/Filters/Filters.jsx
@@ -1,67 +1,74 @@
 import "./Filters.scss";
-import InputMobile from "../Formulaires/InputMobile";
 
 export default function Filters() {
   return (
-    <div className="Filters_Container">
-      <form>
-        <label htmlFor="brand" className="brandLabel">
-          <InputMobile
-            placeholder="Marque"
-            name="brand"
-            id="brand"
-            autoComplete="brand"
-          />
+    <section className="Filters_Container">
+      <form className="Filters_form">
+        <h2 className="Filters-title">Tri par Filtre</h2>
+        <label htmlFor="brand" className="Filter_label">
+          <select name="brand" className="Filter_select">
+            <option value="">Marque</option>
+            <option value="dog">Value 1</option>
+            <option value="cat">Value 2</option>
+            <option value="hamster">Value 3</option>
+            <option value="parrot">Value 4</option>
+          </select>
         </label>
-        <label htmlFor="model" className="modelLabel">
-          <InputMobile
-            placeholder="Modèle"
-            name="model"
-            id="model"
-            autoComplete="model"
-          />
+        <label htmlFor="model" className="Filter_label">
+          <select name="model" className="Filter_select">
+            <option value="">modèle</option>
+            <option value="dog">Value 1</option>
+            <option value="cat">Value 2</option>
+            <option value="hamster">Value 3</option>
+            <option value="parrot">Value 4</option>
+          </select>
         </label>
-        <label htmlFor="factory" className="factoryLabel">
-          <InputMobile
-            placeholder="Magasin"
-            name="factory"
-            id="factory"
-            autoComplete="factory"
-          />
+        <label htmlFor="factory" className="Filter_label">
+          <select name="factory" className="Filter_select">
+            <option value="">Magasin</option>
+            <option value="dog">Value 1</option>
+            <option value="cat">Value 2</option>
+            <option value="hamster">Value 3</option>
+            <option value="parrot">Value 4</option>
+          </select>
         </label>
-        <label htmlFor="storage" className="storageLabel">
-          <InputMobile
-            placeholder="Stockage"
-            name="storage"
-            id="storage"
-            autoComplete="storage"
-          />
+        <label htmlFor="storage" className="Filter_label">
+          <select name="storage" className="Filter_select">
+            <option value="">Stockage</option>
+            <option value="dog">Value 1</option>
+            <option value="cat">Value 2</option>
+            <option value="hamster">Value 3</option>
+            <option value="parrot">Value 4</option>
+          </select>
         </label>
-        <label htmlFor="network" className="networkLabel">
-          <InputMobile
-            placeholder="Réseau"
-            name="network"
-            id="network"
-            autoComplete="network"
-          />
+        <label htmlFor="network" className="Filter_label">
+          <select name="network" className="Filter_select">
+            <option value="">Réseau</option>
+            <option value="dog">Value 1</option>
+            <option value="cat">Value 2</option>
+            <option value="hamster">Value 3</option>
+            <option value="parrot">Value 4</option>
+          </select>
         </label>
-        <label htmlFor="catégory" className="catégoryLabel">
-          <InputMobile
-            placeholder="Catégorie"
-            name="catégory"
-            id="catégory"
-            autoComplete="catégory"
-          />
+        <label htmlFor="category" className="Filter_label">
+          <select name="category" className="Filter_select">
+            <option value="">Catégorie</option>
+            <option value="dog">Value 1</option>
+            <option value="cat">Value 2</option>
+            <option value="hamster">Value 3</option>
+            <option value="parrot">Value 4</option>
+          </select>
         </label>
-        <label htmlFor="weighting" className="weightingLabel">
-          <InputMobile
-            placeholder="Pondération"
-            name="weighting"
-            id="weighting"
-            autoComplete="weighting"
-          />
+        <label htmlFor="weighting" className="Filter_label">
+          <select name="weighting" className="Filter_select">
+            <option value="">Pondération</option>
+            <option value="dog">Value 1</option>
+            <option value="cat">Value 2</option>
+            <option value="hamster">Value 3</option>
+            <option value="parrot">Value 4</option>
+          </select>
         </label>
       </form>
-    </div>
+    </section>
   );
 }
